98e80851354f652d1be7f56a87458ee46c3740d1
Arman .M <Arman.M.12121@Gmail.com>
2022-06-03 03:20:38 +0430

    Welcome Page + Sign Up

--- UI/Views/ShellView.xaml.cs
+++ UI/Views/ShellView.xaml.cs
@@ -22,9 +22,32 @@ namespace UI.Views
 		public ShellView()
 		{
 			InitializeComponent();
-		}
+		} 
 
 		private void SignUpBtn_Click(object sender, RoutedEventArgs e)
+		{
+			Close();
+		}
+
+		private void SignUpOptBtn_Click(object sender, RoutedEventArgs e)
+		{
+			WelcomeGrid.Visibility = Visibility.Collapsed;
+			SignUpGrid.Visibility = Visibility.Visible;
+			UsernameTextBox.Focus();
+		}
+
+		private void SignUpBack_Click(object sender, RoutedEventArgs e)
+		{
+			SignUpGrid.Visibility = Visibility.Collapsed;
+			WelcomeGrid.Visibility = Visibility.Visible;
+		}
+
+		private void LogInOptBtn_Click(object sender, RoutedEventArgs e)
+		{
+			MessageBox.Show("Loggen in successfuly!");
+		}
+
+		private void QuitBtn_Click(object sender, RoutedEventArgs e)
 		{
 			Close();
 		}
